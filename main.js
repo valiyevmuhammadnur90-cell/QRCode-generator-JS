b27fe000f9564a91de1ac1cc61fdffc807d993d8
Muhammadnur <valiyevmuhammadnur90@gmail.com>
2025-12-28 09:46:30 +0500

    project finished

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 function generateQR() {
-  const qrcode = document.getElementById("qrcode");
-  const text = document.getElementById("text");
+  const qrcode = document.getElementById("qr-code");
+  const input = document.getElementById("qr-input");
 
   if (qrcode.innerHTML !== "") {
     qrcode.innerHTML = "";
   }
+  if (input.value.trim() === "") {
+    alert("Please enter text or URL");
+    return;
+  }
 
   new QRCode(qrcode, {
-    text: text.value,
+    text: input.value,
     width: 200,
     height: 200,
-    colorDark: "transparent",
-    colorLight: "#16a34a",
+    colorDark: "#22c55e",
+    colorLight: "#ffffff",
   });
 }
+
+const downloadBtn = document.getElementById("download-btn");
+
+downloadBtn.addEventListener("click", function () {
+  const qrImage = document.querySelector("#qr-code img");
+
+  if (!qrImage) {
+    alert("Please generate a QR code first!");
+    return;
+  }
+
+  const link = document.createElement("a");
+  link.href = qrImage.src;
+  link.download = "QRCode.png";
+  link.click();
+});
